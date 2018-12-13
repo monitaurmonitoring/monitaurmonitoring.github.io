dccc7bd10aad4185572389e92c9a77616faa8a01
Josh Cox <josh@webhosting.coop>
2018-12-13 07:48:55 -0600

    logo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import g from "glamorous"
 import { css } from "glamor"
 import Link from "gatsby-link"
-import stawkhead from "./stawkhead.jpg";
+import monitaur from "./monitaur.png";
 
 import { rhythm } from "../utils/typography"
 
@@ -24,8 +24,8 @@ export default ({ children, data }) =>
           </Link>
         </div>
       </div>
-      <div className='stawkhead'>
-        <img src={stawkhead} height="442" alt="Stawkhead" className='stawk-center' />
+      <div className='monitaur'>
+        <img src={monitaur} height="442" alt="monitaur" className='monit-center' />
       </div>
       <div className='portal'>
         {children()}
