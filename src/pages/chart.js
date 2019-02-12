e01037f436528f3eabe7793be82827a7e93dbe81
Josh Cox <josh@webhosting.coop>
2019-02-11 21:56:32 -0600

    chart

--- src/pages/chart.js
+++ src/pages/chart.js
@@ -4,7 +4,7 @@ export default () => (
   <div>
     <h2>Chart Monitaur</h2>
     <p>
-    Go <a href=https://chart.monitaur.net/app/kibana#/dashboard/3403e320-2e42-11e9-9a99-9d10d60531b9?_g=(refreshInterval:(pause:!f,value:30000),time:(from:now-7d,mode:quick,to:now))&_a=(description:'005%20DASH',filters:!(),fullScreenMode:!f,options:(darkTheme:!t,hidePanelTitles:!f,useMargins:!t),panels:!((embeddableConfig:(),gridData:(h:15,i:'1',w:24,x:0,y:0),id:'1c8f3fa0-2e42-11e9-9a99-9d10d60531b9',panelIndex:'1',type:visualization,version:'6.5.4')),query:(language:lucene,query:''),timeRestore:!t,title:V005DASH,viewMode:view)>here</a>
+    Go <a href=https://chart.monitaur.net/>here</a>
     </p>
   </div>
 );
